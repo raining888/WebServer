5cc882d6d3bd69b5c25228f09bb8e3a4e4a42bd4
rain <20520@126.com>
2022-11-21 10:51:58 +0800

    fix no type file

--- main.cc
+++ main.cc
@@ -5,59 +5,71 @@
 #include "muduo/net/http/HttpResponse.h"
 #include "muduo/net/http/HttpServer.h"
 
+#include <sys/stat.h>
 #include <iostream>
 #include <map>
 #include <regex>
-#include <sys/stat.h>
 
 using namespace muduo;
 using namespace muduo::net;
 
 bool benchmark = false;
 
-inline bool isDir(const string& path)
+inline bool isDir(const string &path)
 {
   struct stat st;
   return stat(path.c_str(), &st) >= 0 && S_ISDIR(st.st_mode);
 }
 
-inline bool isFile(const string& path)
+inline bool isFile(const string &path)
 {
   struct stat st;
   return stat(path.c_str(), &st) >= 0 && S_ISREG(st.st_mode);
 }
 
-inline bool isValidPath(const string& path) {
+inline bool isValidPath(const string &path)
+{
   size_t level = 0;
   size_t i = 0;
 
   // Skip slash
-  while (i < path.size() && path[i] == '/') {
+  while (i < path.size() && path[i] == '/')
+  {
     i++;
   }
 
-  while (i < path.size()) {
+  while (i < path.size())
+  {
     // Read component
     auto beg = i;
-    while (i < path.size() && path[i] != '/') {
+    while (i < path.size() && path[i] != '/')
+    {
       i++;
     }
 
     auto len = i - beg;
     assert(len > 0);
 
-    if (!path.compare(beg, len, ".")) {
+    if (!path.compare(beg, len, "."))
+    {
       ;
-    } else if (!path.compare(beg, len, "..")) {
-      if (level == 0) {                return false;
+    }
+    else if (!path.compare(beg, len, ".."))
+    {
+      if (level == 0)
+      {
+        return false;
       }
       level--;
-    } else {
+    }
+    else
+    {
       level++;
     }
 
     // Skip slash
-    while (i < path.size() && path[i] == '/') {
+    while (i < path.size() && path[i] == '/')
+    {
       i++;
     }
   }
@@ -65,55 +77,87 @@ inline bool isValidPath(const string& path) {
   return true;
 }
 
-inline string fileExtension(const string &path) {
+inline string fileExtension(const string &path)
+{
   std::smatch m;
   auto pat = std::regex("\\.([a-zA-Z0-9]+)$");
-  if (std::regex_search(path, m, pat)) {
+  if (std::regex_search(path, m, pat))
+  {
     return m[1].str();
   }
   return std::string();
 }
 
-inline const char *findContentType(const string &path) {
+inline const char *findContentType(const string &path)
+{
   auto ext = fileExtension(path);
-  if (ext == "txt") {
+  if (ext == "txt")
+  {
     return "text/plain";
-  } else if (ext == "html") {
+  }
+  else if (ext == "html")
+  {
     return "text/html";
-  } else if (ext == "css") {
+  }
+  else if (ext == "css")
+  {
     return "text/css";
-  } else if (ext == "jpeg" || ext == "jpg") {
+  }
+  else if (ext == "jpeg" || ext == "jpg")
+  {
     return "image/jpg";
-  } else if (ext == "png") {
+  }
+  else if (ext == "png")
+  {
     return "image/png";
-  } else if (ext == "gif") {
+  }
+  else if (ext == "gif")
+  {
     return "image/gif";
-  } else if (ext == "svg") {
+  }
+  else if (ext == "svg")
+  {
     return "image/svg+xml";
-  } else if (ext == "ico") {
+  }
+  else if (ext == "ico")
+  {
     return "image/x-icon";
-  } else if (ext == "json") {
+  }
+  else if (ext == "json")
+  {
     return "application/json";
-  } else if (ext == "pdf") {
+  }
+  else if (ext == "pdf")
+  {
     return "application/pdf";
-  } else if (ext == "js") {
+  }
+  else if (ext == "js")
+  {
     return "application/javascript";
-  } else if (ext == "xml") {
+  }
+  else if (ext == "xml")
+  {
     return "application/xml";
-  } else if (ext == "xhtml") {
+  }
+  else if (ext == "xhtml")
+  {
     return "application/xhtml+xml";
-  } else if (ext == "mp4") {
+  }
+  else if (ext == "mp4")
+  {
     return "video/mp4";
   }
-  return nullptr;
+  return "";
 }
 
-void onRequest(const HttpRequest &req, HttpResponse *resp) {
-  std::cout << "Headers " << req.methodString() << " " << req.path()
-    << std::endl;
-  if (!benchmark) {
+void onRequest(const HttpRequest &req, HttpResponse *resp)
+{
+  std::cout << "Headers " << req.methodString() << " " << req.path() << std::endl;
+  if (!benchmark)
+  {
     const std::map<string, string> &headers = req.headers();
-    for (const auto &header : headers) {
+    for (const auto &header : headers)
+    {
       std::cout << header.first << ": " << header.second << std::endl;
     }
   }
@@ -121,9 +165,8 @@ void onRequest(const HttpRequest &req, HttpResponse *resp) {
   string rootDir = "./root";
   string path = rootDir + req.path();
 
-  if (isDir(rootDir) &&
-      isValidPath(req.path()) &&
-      isFile(path)) {
+  if (isDir(rootDir) && isValidPath(req.path()) && isFile(path))
+  {
     resp->setStatusCode(HttpResponse::k200Ok);
     resp->setStatusMessage("OK");
     auto type = findContentType(path);
@@ -133,20 +176,25 @@ void onRequest(const HttpRequest &req, HttpResponse *resp) {
     struct stat st;
     stat(path.c_str(), &st);
     int err = FileUtil::readFile(path, 102400000, &res, &st.st_size);
-    if (err) {
+    if (err)
+    {
       std::cout << "read file error" << std::endl;
     }
     resp->setBody(res);
-  } else {
+  }
+  else
+  {
     resp->setStatusCode(HttpResponse::k404NotFound);
     resp->setStatusMessage("Not Found");
     resp->setCloseConnection(true);
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   int numThreads = 0;
-  if (argc > 1) {
+  if (argc > 1)
+  {
     benchmark = true;
     Logger::setLogLevel(Logger::WARN);
     numThreads = atoi(argv[1]);
